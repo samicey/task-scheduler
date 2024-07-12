a60793a1669c2fe67756bd68ebbd541441e437a7
Samuel Obikaonu <samuelchibuike22@gmail.com>
2024-07-12 04:59:12 +0200

    add validation middleware

---
+++ src/middlewares/validations.middleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+export const validateScheduleRequestSchema = (request: Request, _response: Response, next: NextFunction): void => {
+  const scheduleRequestSchema = Joi.object({
+    id: Joi.string(),
+    userId: Joi.string().required(),
+    day: Joi.string().required(),
+    startTime: Joi.string().required(),
+    endTime: Joi.string().required(),
+  });
+
+  const { error } = scheduleRequestSchema.validate(request.body);
+
+  if (!error) {
+    next();
+  } else {
+    _response.status(400).json({ message: error.message, success: false });
+  }
+};
+
+export const validateScheduleRequestParameter = (request: Request, _response: Response, next: NextFunction): void => {
+    const scheduleRequestParameter = Joi.object({
+      userId: Joi.string().required(),
+      scheduleId: Joi.string().required()
+    });
+  
+    const { error } = scheduleRequestParameter.validate(request.params);
+  
+    if (!error) {
+      next();
+    } else {
+        _response.status(400).json({ message: error.message, success: false });
+    }
+  };
